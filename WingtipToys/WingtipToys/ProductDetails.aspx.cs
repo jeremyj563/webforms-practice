dabfa00b0fa4fc1506d5b69c382c1ddd31130e42
Jeremy Johnson <modernclassicgamer@gmail.com>
2019-05-19 22:40:16 -0500

    finished product details implementation

--- WingtipToys/WingtipToys/ProductDetails.aspx.cs
+++ WingtipToys/WingtipToys/ProductDetails.aspx.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.ModelBinding;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WingtipToys.Models;
 
 namespace WingtipToys
 {
@@ -12,6 +14,19 @@ namespace WingtipToys
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public Product GetProduct([QueryString("productID")] int? productID)
+        {
+            var _db = new ProductContext();
+
+            Product product = null;
+            if (productID.HasValue && productID > 0)
+            {
+                product = _db.Products.SingleOrDefault(p => p.ProductID == productID);
+            }
+
+            return product;
         }
     }
 }
--- WingtipToys/WingtipToys/ProductList.aspx.cs
+++ WingtipToys/WingtipToys/ProductList.aspx.cs
@@ -1,17 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using System.Web.ModelBinding;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WingtipToys.Models;
 
 namespace WingtipToys
 {
-    public partial class ProductList : System.Web.UI.Page
+    public partial class ProductList : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            
+        }
 
+        public IQueryable<Product> GetProducts([QueryString("id")]int? categoryID)
+        {
+            var _db = new ProductContext();
+            var products = _db.Products.AsQueryable();
+            if (categoryID.HasValue && categoryID > 0)
+            {
+                products = products.Where(p => p.CategoryID == categoryID);
+            }
+
+            return products;
         }
     }
 }
